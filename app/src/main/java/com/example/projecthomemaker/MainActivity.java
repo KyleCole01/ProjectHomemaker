5d0bf3616cd8470cedc192b7dcbb65534c77cd83
KyleCole01 <kyle@misfitselectronics.com>
2019-05-22 18:08:04 -0400

    Implemented a Media Player and used a static resource audio file to play.

--- app/src/main/java/com/example/projecthomemaker/MainActivity.java
+++ app/src/main/java/com/example/projecthomemaker/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.projecthomemaker;
 
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +12,7 @@ import android.widget.ImageButton;
 public class MainActivity extends AppCompatActivity {
     ImageButton button;
     Context context;
+    MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +24,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(context, RecipeListActivity.class);
+                //plays media sound before going to new intent
+                mediaPlayer = MediaPlayer.create(context,R.raw.are_you_kidding);
+                mediaPlayer.start();
+                mediaPlayer.setLooping(false);
                 startActivity(intent);
 //                public Recipe(String name, String category, int starRating, int feedPerBatch,int costRating,String ingredientList,String directions)
 
